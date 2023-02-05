39a94e409130c31709bff97672a25b318e0fee63
sriVishalS <vishalS221202@gmail.com>
2023-02-05 21:02:14 +0530

    app

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.querySelector(".loaderWrapper").style.display = "none";
+}
 let now_playing = document.querySelector(".now-playing");
 let track_art = document.querySelector(".track-art");
 let track_name = document.querySelector(".track-name");
@@ -199,10 +208,6 @@ function random_bg_color() {
   let Color1 = populate("#");
   let Color2 = populate("#");
   var angle = "to right";
-
-  let gradient =
-    "linear-gradient(" + angle + "," + Color1 + ", " + Color2 + ")";
-  document.body.style.background = gradient;
 }
 function reset() {
   curr_time.textContent = "00:00";
